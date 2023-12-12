e721a1edfcbbeea8fc09011fe901a27c9570d919
Joan Menendez Calafell <jmenendezc@alumnes.santjosepobrer.es>
2023-12-12 17:35:01 +0100

    wow

---
+++ Node/3.02/Actividad1/script.js
@@ -0,0 +1,8 @@
+function LetraGrande() {
+    var element = document.body;
+    element.classList.toggle("letra-grande");
+}
+function LetraPequeña() {
+    var element = document.body;
+    element.classList.toggle("letra-pequeña");
+}
